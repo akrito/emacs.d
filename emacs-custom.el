57cb3e68f08e6c630624623b5462b4ea58de2cf1
Alex Kritikos <alex@archbook.localdomain>
2009-04-20 14:43:44 -0500

    Move some vars from emacs-custom to init

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,19 +3,11 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(custom-raised-buttons nil)
- '(echo-keystrokes 0.01)
  '(fringe-mode 0 nil (fringe))
  '(ido-work-directory-list-ignore-regexps (quote ("^/s/")))
- '(inhibit-startup-screen t)
- '(make-backup-files nil)
  '(mode-line-format (quote ("%e" (buffer-file-truename "%f" "%b") #("-%*-" 0 4 (auto-composed t)) (vc-mode vc-mode) #("--" 0 2 (auto-composed t)) mode-line-modes #("%-" 0 2 (auto-composed t)))))
- '(mode-line-inverse-video nil)
- '(mouse-wheel-progressive-speed nil)
  '(mouse-wheel-scroll-amount (quote (2 ((shift) . 1) ((control)))))
  '(org-agenda-files (quote ("~/wiki/wiki.org_archive")) t)
- '(org-highlight-sparse-tree-matches nil)
- '(show-paren-mode t)
  '(show-trailing-whitespace t)
  '(special-display-buffer-names (quote ("*shell*" "*Shell Command Output*")))
  '(transient-mark-mode t)
