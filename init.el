7bb731f483e1a31b509e9f1b5bfb57193d56dfd4
Alex Kritikos <alex@archbook.localdomain>
2009-06-03 06:07:09 -0500

    added pyflakes support

--- init.el
+++ init.el
@@ -148,3 +148,16 @@
 (add-hook 'yaml-mode-hook
           '(lambda ()
              (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+;; python + pyflakes
+(load "flymake" t)
+(defun flymake-pyflakes-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "pyflakes" (list local-file))))
+(add-to-list 'flymake-allowed-file-name-masks
+             '("\\.py\\'" flymake-pyflakes-init))
+(add-hook 'find-file-hook 'flymake-find-file-hook)
