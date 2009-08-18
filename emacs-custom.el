aa97e076650aef895c69106e68a0490bafe6a935
Alex Kritikos <alex@archbook.localdomain>
2009-08-18 16:35:39 -0500

    Use chromium as the default browser

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,6 +3,8 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-generic))
+ '(browse-url-generic-program "chromium")
  '(fringe-mode 0 nil (fringe))
  '(ido-work-directory-list-ignore-regexps (quote ("^/s/")))
  '(mode-line-format (quote ("%e" (buffer-file-truename "%f" "%b") #("-%*-" 0 4 (auto-composed t)) (vc-mode vc-mode) #("--" 0 2 (auto-composed t)) mode-line-modes #("%-" 0 2 (auto-composed t)))))
