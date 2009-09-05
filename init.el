4c7295537ff73e9519bbaea99c59f0d1098ee032
Alex Kritikos <alex@archbook.localdomain>
2009-09-05 18:07:14 -0500

    email

--- init.el
+++ init.el
@@ -38,7 +38,6 @@
   (find-alternate-file (concat "/sudo:root@localhost:" (buffer-file-name (current-buffer)))))
 (global-set-key (kbd "C-c C-r") 'sudo-edit-current-file)
 
-
 ;; On X11, change the pointer to an arrow
 (if (boundp 'x-pointer-arrow)
     (progn
@@ -49,6 +48,7 @@
 ;; smtp mail
 ;; http://obfuscatedcode.wordpress.com/2007/04/26/configuring-emacs-for-gmails-smtp/
 (setq user-mail-address "alex.kritikos@gmail.com")
+(setq user-full-name "Alex Kritikos")
 (setq send-mail-function 'smtpmail-send-it
       message-send-mail-function 'smtpmail-send-it
       smtpmail-starttls-credentials
@@ -62,6 +62,58 @@
 (autoload 'compose-mail "smtpmail")
 (load "abbrevs" 'noerror)
 (add-hook 'mail-mode-hook 'mail-abbrevs-setup)
+
+;; wanderlust
+(autoload 'wl "wl" "Wanderlust" t)
+(autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
+(autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
+
+;; IMAP
+;;(setq elmo-imap4-default-server "imap.gmail.com")
+;;(setq elmo-imap4-default-user "alex.kritikos@gmail.com")
+;;(setq elmo-imap4-default-authenticate-type 'clear)
+;;(setq elmo-imap4-default-port '993)
+;;(setq elmo-imap4-default-stream-type 'ssl)
+;;(setq elmo-imap4-use-modified-utf7 t)
+
+(autoload 'wl-user-agent-compose "wl-draft" nil t)
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'wl-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'wl-user-agent
+      'wl-user-agent-compose
+      'wl-draft-send
+      'wl-draft-kill
+      'mail-send-hook))
+
+;; bbdb
+(setq bbdb-file "~/.emacs.d/bbdb")           ;; keep ~/ clean; set before loading
+(require 'bbdb)
+(bbdb-initialize)
+(setq
+    bbdb-offer-save 1                        ;; 1 means save-without-asking
+    bbdb-use-pop-up t                        ;; allow popups for addresses
+    bbdb-electric-p t                        ;; be disposable with SPC
+    bbdb-popup-target-lines  1               ;; very small
+    bbdb-dwim-net-address-allow-redundancy t ;; always use full name
+    bbdb-quiet-about-name-mismatches 2       ;; show name-mismatches 2 secs
+    bbdb-always-add-address t                ;; add new addresses to existing
+                                             ;; contacts automatically
+    bbdb-canonicalize-redundant-nets-p t     ;; x@foo.bar.cx => x@bar.cx
+    bbdb-completion-type nil                 ;; complete on anything
+    bbdb-complete-name-allow-cycling t       ;; cycle through matches
+                                             ;; this only works partially
+    bbbd-message-caching-enabled t           ;; be fast
+    bbdb-use-alternate-names t               ;; use AKA
+    bbdb-elided-display t                    ;; single-line addresses
+
+    ;; auto-create addresses from mail
+    bbdb/mail-auto-create-p 'bbdb-ignore-some-messages-hook
+    bbdb-ignore-some-messages-alist ;; don't ask about fake addresses
+    ;; NOTE: there can be only one entry per header (such as To, From)
+    ;; http://flex.ee.uec.ac.jp/texi/bbdb/bbdb_11.html
+    '(( "From" . "no.?reply\\|DAEMON\\|daemon\\|facebookmail\\|twitter")))
 
 ;; github gists
 (require 'gist)
