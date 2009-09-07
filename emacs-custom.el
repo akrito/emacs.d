45f8969bbcb9e3982f9edc35e9f78f7c39c9c2ac
Alex Kritikos <alex@archbook.localdomain>
2009-09-07 15:31:07 -0500

    Made trailing whitespace identifier less annoying

--- emacs-custom.el
+++ emacs-custom.el
@@ -9,10 +9,10 @@
  '(ido-work-directory-list-ignore-regexps (quote ("^/s/")))
  '(mode-line-format (quote ("%e" (buffer-file-truename "%f" "%b") #("-%*-" 0 4 (auto-composed t)) (vc-mode vc-mode) #("--" 0 2 (auto-composed t)) mode-line-modes #("%-" 0 2 (auto-composed t)))))
  '(mouse-wheel-scroll-amount (quote (2 ((shift) . 1) ((control)))))
- '(org-agenda-files (quote ("~/wiki/wiki.org_archive"))))
+ '(org-agenda-files (quote ("~/wiki/wiki.org_archive")) t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(trailing-whitespace ((((class color) (background light)) (:strike-through "lightgray")))))
