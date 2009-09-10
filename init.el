759da87c45386e296cd719d3d297f7f8cd6525dd
Alex Kritikos <alex@archbook.localdomain>
2009-09-10 12:08:43 -0500

    know .yaml is a valid yaml file extension

--- init.el
+++ init.el
@@ -247,6 +247,7 @@
 ;; yaml
 (autoload 'yaml-mode "yaml-mode" "YAML Ain't Markup Language" t)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 (add-hook 'yaml-mode-hook
           '(lambda ()
              (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
