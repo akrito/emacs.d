7228c41a4ac4679b976847aae931c21636461cff
Alex Kritikos <alex@8bitb.us>
2010-04-26 14:57:00 -0500

    Added a shortcut for magit

--- init.el
+++ init.el
@@ -49,7 +49,8 @@
 
 ;; magit
 (autoload 'magit-status "magit" nil t)
-
+(global-set-key (kbd "M-m") 'magit-status)
+ 
 ; ;; iedit
 ; (autoload 'iedit-mode "iedit" nil t)
 ; (define-key global-map (kbd "C-;") 'iedit-mode)
