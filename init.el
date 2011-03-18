06b80e932bf15c851583213caaef7b5cc09010cf
Alex Kritikos <alex@8bitb.us>
2011-03-18 13:19:13 -0500

    Cleaned up keybindings quite a bit. Removed unused libraries.

--- init.el
+++ init.el
@@ -11,6 +11,8 @@
 (setq custom-file "~/.emacs.d/emacs-custom.el")
 (load custom-file 'noerror)
 
+(load "keybindings")
+
 ;; Manual customizations
 (ansi-color-for-comint-mode-on)
 (blink-cursor-mode -1)
@@ -19,12 +21,13 @@
 (global-hl-line-mode 1)
 (server-start)
 (set-fringe-mode 1)
-(scroll-bar-mode 'none)
+(scroll-bar-mode -1)
 (setq auto-save-list-file-prefix nil
       bookmark-save-flag 1
       browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "chromium-browser"
       custom-raised-buttons nil
+      focus-follows-mouse t
       frame-title-format '((buffer-file-truename "%f" "%b"))
       echo-keystrokes 0.01
       gist-view-gist t
@@ -37,6 +40,7 @@
       mouse-wheel-progressive-speed nil
       mouse-wheel-scroll-amount '(2 ((shift) . 1) ((control)))
       org-agenda-files '("~/wiki/wiki.org_archive")
+      ring-bell-function 'ignore
       show-trailing-whitespace t
       speedbar-hide-button-brackets-flag t
       speedbar-indentation-width 2
@@ -47,32 +51,19 @@
       truncate-partial-width-windows nil
       vc-follow-symlinks t
       visible-bell nil)
-(setq-default ispell-program-name "aspell")
-(setq ring-bell-function 'ignore)
 (setq-default fill-column 79
               cursor-type 'bar
+              ispell-program-name "aspell"
               mode-line-format '("%e" (buffer-file-truename "%f" "%b") "-%*--%l--" mode-line-modes "%-")
               indent-tabs-mode nil)
-              ;; show-trailing-whitespace t)
 (show-paren-mode t)
 (tool-bar-mode -1)
-(add-hook 'after-save-hook
-  'executable-make-buffer-file-executable-if-script-p)
-;; Mac-specific, but shouldn't hurt Linux
-(setq mac-command-modifier (quote meta))
-(setq mac-option-modifier (quote alt))
 
 ;; auto-pair parentheses
 (require 'autopair)
 (autopair-global-mode 1)
 (setq autopair-autowrap t)
-;; (put 'autopair-insert-opening 'delete-selection t)
-;; (put 'autopair-skip-close-maybe 'delete-selection t)
-;; (put 'autopair-insert-or-skip-quote 'delete-selection t)
-;; (put 'autopair-extra-insert-opening 'delete-selection t)
-;; (put 'autopair-extra-skip-close-maybe 'delete-selection t)
 (put 'autopair-backspace 'delete-selection 'supersede)
-;; (put 'autopair-newline 'delete-selection t)
 ;; but not in interactive shells
 (defun comint-hook ()
   (setq autopair-dont-activate t))
@@ -89,8 +80,6 @@
 (add-to-list 'term-unbind-key-list "ESC")
 (add-to-list 'term-bind-key-alist '("M-v" . term-paste))
 (add-to-list 'term-bind-key-alist '("M-ESC" . term-send-raw-meta)) 
-;; (add-to-list 'term-bind-key-alist '("M-w" . term-send-backward-kill-word))
-;; (add-to-list 'term-bind-key-alist '("M-e" . term-send-backward-kill-word))
 (setq
  ;; term-default-bg-color "#191717"
  ;; term-default-fg-color "#D2DEC4"
@@ -100,7 +89,8 @@
  ;; For some reason, Ubuntu has terminfo entries for Eterm* but not eterm*
  term-term-name "Eterm-color")
 
-;; Mouse stuff
+;; GUI stuff
+
 ;; On X11, change the pointer to an arrow, and remove the menu bar
 (if (boundp 'x-pointer-arrow)
     (progn
@@ -108,6 +98,10 @@
       ;; hack to force the pointer shape to change
       (set-mouse-color "black")
       (menu-bar-mode 0)))
+
+;; Mac-specific, but shouldn't hurt Linux
+(setq mac-command-modifier (quote meta))
+(setq mac-option-modifier (quote alt))
 
 ;; Some Acme-style chords
 (require 'acme-mouse)
@@ -133,7 +127,6 @@
 (color-theme-emacs-nw)
 ;; (autoload 'global-pretty-mode "pretty-mode" "Pretty mode" t)
 
-;; Completion
 ;; Auto-complete
 (add-to-list 'load-path "~/.emacs.d/auto-complete-1.3")
 (require 'auto-complete-config)
@@ -147,121 +140,6 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete-1.3/dict")
 (add-to-list 'ac-modes 'yaml-mode)
 (global-auto-complete-mode 1)
-
-;; New ergonomic bindings
-;; http://xahlee.org/emacs/ergonomic_emacs_keybinding.html
-(load "ergonomic_keybinding_qwerty")
-;; I don't like all of them
-(global-unset-key (kbd "C-p")) ; Print
-(global-set-key (kbd "C-!") 'shell-command)
-(global-set-key (kbd "C-|") 'shell-command-on-region)
-(global-set-key (kbd "C-a") 'beginning-of-line)
-(global-set-key (kbd "C-n") 'make-frame-command)
-(global-set-key (kbd "C-b") 'switch-to-buffer)
-(global-set-key (kbd "C-e") 'end-of-line)
-(global-set-key (kbd "C-k") 'kill-line)
-(global-set-key (kbd "M-b") 'bookmark-jump)
-(global-set-key (kbd "M-f") 'isearch-forward)
-(global-set-key (kbd "M-h") 'backward-char)
-(global-set-key (kbd "M-l") 'forward-char)
-(global-set-key (kbd "M-k") 'previous-line)
-(global-set-key (kbd "C-k") 'kill-line)
-(global-set-key (kbd "M-j") 'next-line)
-(global-set-key (kbd "M-g") 'goto-line)
-(global-set-key (kbd "M-m") 'magit-status)
-(global-set-key (kbd "M-o") 'find-file)
-(global-set-key (kbd "M-F") 'rgrep)
-(global-set-key (kbd "M-s") 'save-buffer)
-(global-set-key (kbd "C-s") 'other-frame)
-(global-set-key (kbd "M-t") 'revert-buffer)
-(global-set-key (kbd "C-t") 'multi-term-dedicated-toggle)
-(global-set-key (kbd "M-w") 'close-current-buffer)
-(normal-erase-is-backspace-mode 1)
-(global-set-key (kbd "C-d") 'delete-char)
-(global-set-key (kbd "M-3") 'split-window-horizontally)
-(global-set-key (kbd "M-0") 'delete-window)
-(global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
-(global-set-key
-     "\M-a"
-     (lambda ()
-       (interactive)
-       (call-interactively
-        (intern
-         (ido-completing-read
-          "M-a "
-          (all-completions "" obarray 'commandp))))))
-(add-hook 'html-mode-hook
-          '(lambda ()
-             (define-key html-mode-map "\M-s" 'save-buffer)))
-(add-hook 'org-mode-hook
-          '(lambda ()
-             (define-key org-mode-map "\M-a" (lambda ()
-       (interactive)
-       (call-interactively
-        (intern
-         (ido-completing-read
-          "M-a "
-          (all-completions "" obarray 'commandp))))))
-             (define-key org-mode-map "\M-s" 'save-buffer)))
-(defun ido-goto-symbol (&optional symbol-list)
-  "Refresh imenu and jump to a place in the buffer using Ido."
-  (interactive)
-  (unless (featurep 'imenu)
-    (require 'imenu nil t))
-  (cond
-   ((not symbol-list)
-    (let ((ido-mode ido-mode)
-          (ido-enable-flex-matching
-           (if (boundp 'ido-enable-flex-matching)
-               ido-enable-flex-matching t))
-          name-and-pos symbol-names position)
-      (unless ido-mode
-        (ido-mode 1)
-        (setq ido-enable-flex-matching t))
-      (while (progn
-               (imenu--cleanup)
-               (setq imenu--index-alist nil)
-               (ido-goto-symbol (imenu--make-index-alist))
-               (setq selected-symbol
-                     (ido-completing-read "Symbol? " symbol-names))
-               (string= (car imenu--rescan-item) selected-symbol)))
-      (unless (and (boundp 'mark-active) mark-active)
-        (push-mark nil t nil))
-      (setq position (cdr (assoc selected-symbol name-and-pos)))
-      (cond
-       ((overlayp position)
-        (goto-char (overlay-start position)))
-       (t
-        (goto-char position)))))
-   ((listp symbol-list)
-    (dolist (symbol symbol-list)
-      (let (name position)
-        (cond
-         ((and (listp symbol) (imenu--subalist-p symbol))
-          (ido-goto-symbol symbol))
-         ((listp symbol)
-          (setq name (car symbol))
-          (setq position (cdr symbol)))
-         ((stringp symbol)
-          (setq name symbol)
-          (setq position
-                (get-text-property 1 'org-imenu-marker symbol))))
-        (unless (or (null position) (null name)
-                    (string= (car imenu--rescan-item) name))
-          (add-to-list 'symbol-names name)
-          (add-to-list 'name-and-pos (cons name position))))))))
-(global-set-key (kbd "M-r") 'ido-goto-symbol) ; or any key you see fit
-(require 'find-file-in-project)
-(defun ido-find-file-in-tag-files ()
-      (interactive)
-      (save-excursion
-        (let ((enable-recursive-minibuffers t))
-          (visit-tags-table-buffer))
-        (find-file
-         (expand-file-name
-          (ido-completing-read
-           "Project file: " (tags-table-files) nil t)))))
-(global-set-key (kbd "M-p") 'ido-find-file-in-tag-files)
 
 ;; better buffer names
 (require 'uniquify)
@@ -307,7 +185,7 @@
 ;; Use the default virtualenv
 (workon-postactivate "~/v/ellington")
 (add-hook 'python-mode-hook 'imenu-add-menubar-index)
-(setq python-mode-hook nil)
+;; (setq python-mode-hook nil)
 
 ;; Haskell
 (autoload 'haskell-mode "~/.emacs.d/haskell-mode/haskell-site-file" "Haskell mode" t)
@@ -350,17 +228,15 @@
 )
 (add-hook 'LaTeX-mode-hook 'tex-hook)
 
-;; Currently disabled
+;; Varnish conf support
+(autoload 'vcl-mode "vcl-mode" "Edit Varnish VCL files" t)
+(add-to-list 'auto-mode-alist '("\\.vcl$" . vcl-mode))
 
-; ;; Varnish conf support
-; (autoload 'vcl-mode "vcl-mode" "Edit Varnish VCL files" t)
-; (add-to-list 'auto-mode-alist '("\\.vcl$" . vcl-mode))
-;
-; ;; Lua support
-; (autoload 'lua-mode "lua-mode" "Edit Lua scripts" t)
-; (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+;; Lua support
+(autoload 'lua-mode "lua-mode" "Edit Lua scripts" t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 
-;; clojure-mode
+;; ;; clojure-mode
 ;; (add-to-list 'load-path "~/opt/clojure-mode")
 ;; (require 'clojure-mode)
 
