546d560806180e0ec0aea34666fef498a11e3d1e
Alex Kritikos <alex@8bitb.us>
2011-11-17 14:38:30 -0600

    Keep it all in one big file. It's org-mode, so who cares?

--- linux.el
+++
@@ -1,8 +0,0 @@
-;; On X11, change the pointer to an arrow, and remove the menu bar
-(if (boundp 'x-pointer-arrow)
-    (progn
-      (setq-default x-pointer-shape x-pointer-arrow)
-      ;; hack to force the pointer shape to change
-      (set-mouse-color "black")
-      (menu-bar-mode 0)))
-(set-face-attribute 'default nil :font "Ubuntu Mono-7.5")
--- osx.el
+++
@@ -1,4 +0,0 @@
-;; Mac-specific, but shouldn't hurt Linux
-(setq mac-command-modifier (quote meta))
-(setq mac-option-modifier (quote alt))
-(set-default-font "Inconsolata-13")
